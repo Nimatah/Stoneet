883b119f6b206aedbec9634b48104754b445928a
tajious <alireza7192@gmail.com>
2020-11-12 12:28:09 +0330

    change login method, fix password input

--- apps/users/models.py
+++ apps/users/models.py
@@ -82,7 +82,7 @@ class User(AbstractBaseUser, PermissionsMixin, TimestampedModel):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     objects = UserManager()
 
     def __str__(self) -> str:
