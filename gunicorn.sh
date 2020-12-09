b3b888c325ebb96eba911ddd78ac9f0a3ab1d8fc
tajious <alireza7192@gmail.com>
2020-12-09 12:37:04 +0330

    change configuration

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,9 +1,10 @@
 NAME="project"
 DJANGODIR=$PWD
-BIND=0.0.0.0:8000
+BIND=0.0.0.0
+PORT=8000
 USER=root
 GROUP=$USER
-WORKERS_COUNT=3
+WORKERS_COUNT=1
 DJANGO_WSGI_MODULE=project.wsgi
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
@@ -12,4 +13,4 @@ exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --user=${USER}
   --group=${GROUP} \
   --log-level=debug \
-  --bind=${BIND}
+  --bind=${BIND}:${PORT}
