db266a55da8469b830d29b2abe36d7e71053aad7
tajious <alireza7192@gmail.com>
2020-12-09 14:56:11 +0330

    change configuration

--- gunicorn.sh
+++ gunicorn.sh
@@ -10,7 +10,7 @@ DJANGO_WSGI_MODULE=project.wsgi
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name ${NAME} \
   --workers ${WORKERS_COUNT} \
-  --user=${USER}
-  --group=${GROUP} \
-  --log-level=debug \
-  --bind=${BIND}:${PORT}
+  --user ${USER}
+  --group ${GROUP} \
+  --log-level debug \
+  --bind ${BIND}:${PORT}
