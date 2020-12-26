cdb5a79eb88d7e3bf096cca035644b0817190a96
tajious <alireza7192@gmail.com>
2020-12-27 01:25:48 +0330

    changed database to postgresql

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -1,10 +1,13 @@
 import os
 import sys
 
+_local: str = os.getenv("DJANGO_LOCAL", True)
 _debug: str = os.getenv("DJANGO_DEBUG", True)
 
-if _debug:
+if _local:
     from .base import *
+elif _debug:
+    from .staging import *
 else:
     from .production import *
 
--- project/settings/staging.py
+++ project/settings/staging.py
@@ -1,5 +1,18 @@
+import os
+
 from .base import *
 
 USE_X_FORWARDED_HOST = True
 
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'planhub',
+        'USER': 'planhub',
+        'PASSWORD': os.getenv('DJANGO_DATABASE_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+    },
+}
