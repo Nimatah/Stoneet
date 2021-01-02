b6350d45420cb3aacaa3e44bda437f4539979b83
NimaAM <nima.bagheri6486@gmai;.com>
2021-01-03 00:15:39 +0330

    change steps change uploadimg step

--- static/js/add-product.js
+++ static/js/add-product.js
@@ -46,93 +46,32 @@ $('#s-3-btn').on('click', function (e) {
 });
 
 
-function initImageUpload(box) {
-  let uploadField = box.querySelector('.image-upload');
-
-  uploadField.addEventListener('change', getFile);
-
-  function getFile(e){
-    let file = e.currentTarget.files[0];
-    checkType(file);
-  }
-
-  function previewImage(file){
-    let thumb = box.querySelector('.js--image-preview'),
-        reader = new FileReader();
-
-    reader.onload = function() {
-      thumb.style.backgroundImage = 'url(' + reader.result + ')';
-    }
-    reader.readAsDataURL(file);
-    thumb.className += ' js--no-default';
-  }
-
-  function checkType(file){
-    let imageType = /image.*/;
-    if (!file.type.match(imageType)) {
-      throw 'Datei ist kein Bild';
-    } else if (!file){
-      throw 'Kein Bild gewählt';
-    } else {
-      previewImage(file);
-    }
-  }
-
-}
-
-// initialize box-scope
-var boxes = document.querySelectorAll('.box');
-
-for (let i = 0; i < boxes.length; i++) {
-  let box = boxes[i];
-  initDropEffect(box);
-  initImageUpload(box);
-}
 
 
 
-/// drop-effect
-function initDropEffect(box){
-  let area, drop, areaWidth, areaHeight, maxDistance, dropWidth, dropHeight, x, y;
 
-  // get clickable area for drop effect
-  area = box.querySelector('.js--image-preview');
-  area.addEventListener('click', fireRipple);
 
-  function fireRipple(e){
-    area = e.currentTarget
-    // create drop
-    if(!drop){
-      drop = document.createElement('span');
-      drop.className = 'drop';
-      this.appendChild(drop);
-    }
-    // reset animate class
-    drop.className = 'drop';
+      $(".imgAdd").click(function () {
+         $(this).closest(".row").find('.imgAdd').before('<div class="col-6 col-md-4 imgUp"><div class="imagePreview"></div><label class="btn ph-spm-create-produc-img-up-btn">آپلود<input type="file" class="uploadFile img" value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label><i class="fa fa-times del"></i></div>');
+      });
+      $(document).on("click", "i.del", function () {
+         $(this).parent().remove();
+      });
+      $(function () {
+         $(document).on("change", ".uploadFile", function () {
+            var uploadFile = $(this);
+            var files = !!this.files ? this.files : [];
+            if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
 
-    // calculate dimensions of area (longest side)
-    areaWidth = getComputedStyle(this, null).getPropertyValue("width");
-    areaHeight = getComputedStyle(this, null).getPropertyValue("height");
-    maxDistance = Math.max(parseInt(areaWidth, 10), parseInt(areaHeight, 10));
+            if (/^image/.test(files[0].type)) { // only image file
+               var reader = new FileReader(); // instance of the FileReader
+               reader.readAsDataURL(files[0]); // read the local file
 
-    // set drop dimensions to fill area
-    drop.style.width = maxDistance + 'px';
-    drop.style.height = maxDistance + 'px';
+               reader.onloadend = function () { // set image data as background of div
+                  //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+                  uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url(" + this.result + ")");
+               }
+            }
 
-    // calculate dimensions of drop
-    dropWidth = getComputedStyle(this, null).getPropertyValue("width");
-    dropHeight = getComputedStyle(this, null).getPropertyValue("height");
-
-    // calculate relative coordinates of click
-    // logic: click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center
-    x = e.pageX - this.offsetLeft - (parseInt(dropWidth, 10)/2);
-    y = e.pageY - this.offsetTop - (parseInt(dropHeight, 10)/2) - 30;
-
-    // position drop and animate
-    drop.style.top = y + 'px';
-    drop.style.left = x + 'px';
-    drop.className += ' animate';
-    e.stopPropagation();
-
-  }
-}
+         });
+      });
