ca2dc99749c0b149b7be43fc85ffc0787d2da937
mansour <mansour.sarhani@gmail.com>
2021-01-16 18:46:36 +0330

    Updated homepage Added Single Product Page Updated Header Updated ph-master-style.css

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from .views import ProductDetailView
+
+
 app_name = 'products'
 
 urlpatterns = [
+    path('1/', ProductDetailView.as_view(), name='product_details')
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,1 +1,5 @@
 from django.views.generic import TemplateView
+
+
+class ProductDetailView(TemplateView):
+    template_name = 'products/product_details.html'
