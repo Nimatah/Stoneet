35fa31bd9fa7b71611e6fb4e565c3e2aa4339ec6
mansour <mansour.sarhani@gmail.com>
2021-01-19 15:42:12 +0330

    Updated List Product Page list_product.html

--- apps/products/views.py
+++ apps/products/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class ProductDetailView(TemplateView):
     template_name = 'products/product_details.html'
+
+
+class ListProductView(TemplateView):
+    template_name = 'products/list_product.html'
