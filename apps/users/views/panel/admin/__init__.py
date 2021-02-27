465014533c8ad1f825024b15a062c880aeb0aeca
mansour <mansour.sarhani@gmail.com>
2021-02-27 18:39:32 +0330

    Created Add Category Page add_category.html
    Created Manage Categories Page
    list_category.html
    
    Updated List Admins page
    list_admin.html

--- apps/users/views/panel/admin/__init__.py
+++ apps/users/views/panel/admin/__init__.py
@@ -1,3 +1,4 @@
 from .dashboard import AdminDashboardView
 from .user_management import *
 from .product_management import *
+from .category_management import *
---
+++ apps/users/views/panel/admin/category_management/__init__.py
@@ -0,0 +1,3 @@
+from .add_category import AddCategoryView
+from .list_category import ListCategoryView
+
