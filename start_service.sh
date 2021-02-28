949fa613ab7caef7ba85052d82e44a61025a4aed
tajious <alireza7192@gmail.com>
2021-02-28 20:18:59 +0330

    added migrations, added profile fixtures

--- start_service.sh
+++ start_service.sh
@@ -2,5 +2,6 @@ mkdir -p logs
 python manage.py migrate
 python manage.py loaddata attributes.json
 python manage.py loaddata categories.json
+python manage.py loaddata profile.json
 python manage.py loaddata users.json
 supervisord -c /project/config/supervisord.conf
