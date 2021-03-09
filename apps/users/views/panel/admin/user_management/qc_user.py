7bd65173f84f8590ca6c3e636c95300c8d3a97bb
tajious <alireza7192@gmail.com>
2021-03-09 17:08:08 +0330

    add new pages

--- apps/users/views/panel/admin/user_management/qc_user.py
+++ apps/users/views/panel/admin/user_management/qc_user.py
@@ -5,5 +5,13 @@ class QCSellerView(TemplateView):
     template_name = 'users/admin/user_management/qc_user/qc_seller.html'
 
 
+class QCSellerDetailsView(TemplateView):
+    template_name = 'users/admin/user_management/qc_user/qc_list_sellers.html'
+
+
 class QCLogisticView(TemplateView):
     template_name = 'users/admin/user_management/qc_user/qc_logistic.html'
+
+
+class QCLogisticDetailsView(TemplateView):
+    template_name = 'users/admin/user_management/qc_user/qc_list_logistics.html'
