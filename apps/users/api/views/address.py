8c68ff7115c9a13191c4ef402af599febc3e51df
tajious <alireza7192@gmail.com>
2021-03-11 16:32:18 +0330

    fix address api messages

--- apps/users/api/views/address.py
+++ apps/users/api/views/address.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.users.api.serializers import AddressSerializer
 from apps.users.models import Address
@@ -10,6 +12,18 @@ class AddressListCreateAPIView(ListCreateAPIView):
 
     def get_queryset(self):
         return Address.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+        response = {}
+        for k, v in serializer.errors.items():
+            response[serializer.fields[f'{k}'].label] = v
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AddressRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
--- apps/users/api/views/mine.py
+++ apps/users/api/views/mine.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.users.api.serializers import MineSerializer
 from apps.users.models import Mine
@@ -11,6 +13,17 @@ class MineListCreateAPIView(ListCreateAPIView):
     def get_queryset(self):
         return Mine.objects.filter(user=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=False):
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+        response = {}
+        for k, v in serializer.errors.items():
+            response[serializer.fields[f'{k}'].label] = v
+        return Response(data=response, status=status.HTTP_400_BAD_REQUEST)
 
 class MineRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
 
