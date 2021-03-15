952c1edb4d1087ad3b21501bf5ee8bade9305f41
NimaAM <nima.bagheri6486@gmai;.com>
2021-03-15 18:00:13 +0330

    update add_product.html admin panel update css list_invoice.html update css list_order.html create view_order.html seller panel

--- apps/users/views/panel/seller/orders/list_order.py
+++ apps/users/views/panel/seller/orders/list_order.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 from apps.orders.models import Order
+from apps.products.models import Attribute
 
 
 class ListOrderView(UserPassesTestMixin, ListView):
@@ -13,12 +14,11 @@ class ListOrderView(UserPassesTestMixin, ListView):
 
     def get_queryset(self):
         queryset = Order.objects.filter(product__user=self.request.user)
-        # queryset = ProductFilter(self.request.GET, queryset=queryset).qs
         return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['categories'] = [c.to_dict_hierarchy() for c in Category.objects.get_root().prefetch_related('children')]
+        context['payment_map'] = Attribute.PAYMENT_MAP
         return context
 
     def test_func(self):
