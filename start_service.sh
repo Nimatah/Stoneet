d615d3de7e6e23018fe12a9f9393f53305a64ec8
tajious <alireza7192@gmail.com>
2021-03-16 00:00:25 +0330

    update run

--- start_service.sh
+++ start_service.sh
@@ -5,4 +5,5 @@ python manage.py loaddata attributes.json
 python manage.py loaddata categories.json
 python manage.py loaddata users.json
 python manage.py loaddata profile.json
+python manage.py loaddata statics
 supervisord -c /project/config/supervisord.conf
