b3e64fcc006a979d161571c610f12718d23fdf74
tajious <alireza7192@gmail.com>
2021-04-07 15:48:40 +0430

    add admin list product and WTF(css)??

--- apps/users/views/panel/admin/product_management/list_product.py
+++ apps/users/views/panel/admin/product_management/list_product.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from apps.products.models import Product
 
 
-class ListProductView(TemplateView):
+class ListProductView(ListView):
+
     template_name = 'users/admin/product_management/list_product.html'
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 10
+    page_kwarg = 'p'
+
+    def test_func(self):
+        return self.request.user.is_authenticated and (self.request.user.is_admin or self.request.user.is_superuser)
